eaa649a7a4303b618e3cab9e11ae96b1e90273e3
Edivan de Barros Galindo <edivan.galindo@gmail.com>
2019-07-10 21:50:43 -0300

    Add things II

--- src/components/WhatsappSend/index.js
+++ src/components/WhatsappSend/index.js
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
-import TextField from '@atlaskit/textfield';
-import Button, { ButtonGroup } from '@atlaskit/button';
-import FieldTextArea from '@atlaskit/field-text-area/FieldTextArea';
-import TextArea from '@atlaskit/textarea';
-import Form, { Field, FormFooter, HelperMessage } from '@atlaskit/form';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import TextArea from "@atlaskit/textarea";
+import TextField from "@atlaskit/textfield";
+import Button, { ButtonGroup } from "@atlaskit/button";
+import Form, { Field, FormFooter, HelperMessage } from "@atlaskit/form";
+import styled from "styled-components";
 
 const ContentWrapper = styled.div`
   display: flex;
-  width: 400px;
+  width: 500px;
   max-width: 100%;
   margin: 0 auto;
   flex-direction: column;
 `;
 
+const PageTitle = styled.h1`
+  margin-bottom: 5px;
+`;
+
 export default function() {
   const [message, setMessage] = useState(
-    'Olá Fulano de Tal, segue o link com sua proposta: https://www.google.com/'
+    "Olá, segue o link para visualizar sua proposta:\r\nhttps://seusite.com.br/propostas/"
   );
 
-  const sendWhatsappMessage = (phone, message) => {
-    debugger;
+  const sendWhatsappMessage = (phone, message) =>
     window.open(
       `https://api.whatsapp.com/send?phone=${encodeURIComponent(
         phone
       )}&text=${encodeURIComponent(message)}`
     );
-  };
 
-  const removeNonNumerical = str => str.replace(/[\W_]/gi, '');
+  const removeNonNumerical = str => str.replace(/[\W_]/gi, "");
 
   return (
     <ContentWrapper>
+      <PageTitle>Exemplo de envio Whatsapp</PageTitle>
       <Form
-        onSubmit={data => {
-          debugger;
-          sendWhatsappMessage(removeNonNumerical(data.phone), message);
-        }}
+        onSubmit={data =>
+          sendWhatsappMessage(removeNonNumerical(data.phone), message)
+        }
       >
         {({ formProps }) => (
           <form {...formProps}>
@@ -57,7 +58,7 @@ export default function() {
             </Field>
             <p>Texto da mensagem</p>
             <TextArea
-              style={{ width: '100%' }}
+              style={{ width: "100%" }}
               isRequired
               value={message}
               name="message"
